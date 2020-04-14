60a3cbd59c2e80d4938c51f8bf45f7bc5c6d9f2f
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2020-04-14 16:19:59 -0400

    1

--- P26_TopologicalSorting/26.py
+++ P26_TopologicalSorting/26.py
@@ -3,44 +3,49 @@ from collections import defaultdict
 
 class Graph:
     def __init__(self, directed=False):
-        self.graph = defaultdict(list)
+        self.graph = defaultdict(list)  # Create a new dictionary-like object
         self.directed = directed
 
     def addEdge(self, frm, to):
-        self.graph[frm].append(to)
-
-        if self.directed is False:
+        self.graph[frm].append(to)  # {Key:Value} = {frm:list[to]}
+        # defaultdict(<class 'list'>, {1: [2], 2: []})
+        # defaultdict(<class 'list'>, {1: [2], 2: [], 3: [1]})
+        # defaultdict(<class 'list'>, {1: [2], 2: [], 3: [1, 2]})
+        # defaultdict(<class 'list'>, {1: [2], 2: [], 3: [1, 2], 4: [3]})
+        # defaultdict(<class 'list'>, {1: [2], 2: [], 3: [1, 2], 4: [3, 2]})
+        if self.directed is False:  # undirected or directed
             self.graph[to].append(frm)
         else:
             self.graph[to] = self.graph[to]
 
+    # A recursive function used by topologicalSort
     def topoSortvisit(self, s, visited, sortlist):
         visited[s] = True
-
         for i in self.graph[s]:
             if not visited[i]:
                 self.topoSortvisit(i, visited, sortlist)
-
         sortlist.insert(0, s)
+        # [2]  first insert the vertex that have no value(sink)
+        # [1, 2]    then insert the vertex that all value are visited
+        # [3, 1, 2]     and so on
+        # [4, 3, 1, 2]
 
     def topoSort(self):
         visited = {i: False for i in self.graph}
-
+        # {1: False, 2: False, 3: False, 4: False}
         sortlist = []
 
         for v in self.graph:
             if not visited[v]:
                 self.topoSortvisit(v, visited, sortlist)
 
-        # print(sortlist)
         print(' '.join(map(str, sortlist)))
 
 
 if __name__ == '__main__':
     g = Graph(directed=True)
-    with open('rosalind_ts.txt', 'r') as f:
+    with open('1.txt', 'r') as f:
         for i in f.readlines():
             V1, V2 = list(map(int, i.split()))
             g.addEdge(V1, V2)
     g.topoSort()
-
