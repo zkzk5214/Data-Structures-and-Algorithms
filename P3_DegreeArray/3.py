f063856db874e81c04f147bd5cece7d7e59decd6
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2020-04-17 15:01:11 -0400

    1

---
+++ P3_DegreeArray/3.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, directed=False):
+        self.graph = defaultdict(list)  # Create a new dictionary-like object
+        self.directed = directed
+
+    def addEdge(self, frm, to):
+        self.graph[frm].append(to)  # {Key:Value} = {frm:list[to]}
+        # defaultdict(<class 'list'>, {1: [2], 2: [], 3: [1, 2], 4: [3, 2]})
+        if self.directed is False:  # undirected or directed
+            self.graph[to].append(frm)
+        else:
+            self.graph[to] = self.graph[to]
+        return self.graph
+
+
+if __name__ == '__main__':
+    g = Graph(directed=False)
+    with open('rosalind_deg.txt', 'r') as f:
+        for i in f.readlines():
+            V1, V2 = list(map(int, i.split()))
+            s = g.addEdge(V1, V2)
+
+        for i in sorted(s.keys()):
+            print(len(s[i]), end=" ")
