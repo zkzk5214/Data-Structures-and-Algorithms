2aea0fedd4555ca91049ab81051779aabb97eba1
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2020-04-19 19:04:53 -0400

    1

---
+++ P9_BFS/9.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+def compute_paths(adjacent_lists):
+    paths = [None] * len(adjacent_lists)
+    paths[0] = 0
+    q = deque([0])
+    while len(q):
+        head = q.popleft()
+        for adj in adjacent_lists[head]:
+            if paths[adj] is None:
+                paths[adj] = paths[head] + 1
+                q.append(adj)
+    return paths
+
+
+def main():
+    n, e = map(int, input().split())
+    adjacent_lists = [[] for _ in range(n)]
+    for _ in range(e):
+        v1, v2 = map(int, input().split())
+        adjacent_lists[v1 - 1].append(v2 - 1)
+    print(' '.join(map(lambda x: str(x) if x is not None else '-1', compute_paths(adjacent_lists))))
+
+
+if __name__ == '__main__':
+    main()
