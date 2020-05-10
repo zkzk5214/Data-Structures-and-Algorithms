0acdb99bb07346329c7dc34235a4244abc00a333
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2020-05-10 09:58:41 +0800

    remove

--- P29_QuickSort/29.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    with open('rosalind_qs.txt', 'r') as f:
-        for i in f.readlines():
-            input_list = list(map(int, i.split()))
-            s = sorted(input_list)
-            print(' '.join(map(str, s)))
